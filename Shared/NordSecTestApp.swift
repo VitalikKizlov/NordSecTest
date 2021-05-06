c9fa362b283f0bb73927e3a223b8012255f94261
Vitalii Kizlov <vitalii_kizlov@epam.com>
2021-05-06 11:26:57 +0300

    Initial Commit

---
+++ Shared/NordSecTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NordSecTestApp.swift
+//  Shared
+//
+//  Created by Vitalii Kizlov on 06.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct NordSecTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
